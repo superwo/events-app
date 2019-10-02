1a6add9386ae62230b0743f8ed9ff08f1157674a
Radu <avadanii@tiscali.it>
2019-10-02 15:40:15 +0300

    PlaceInput component

--- src/features/testarea/TestComponent.jsx
+++ src/features/testarea/TestComponent.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { incrementCounter, decrementCounter } from './testActions';
 import { Button } from 'semantic-ui-react';
+import TestPlaceInput from './TestPlaceInput';
 
 const mapState = state => ({
   data: state.test.data
@@ -21,6 +22,9 @@ export class TestComponent extends Component {
         <h3>The answer is: {data}</h3>
         <Button onClick={incrementCounter} positive content='Increment' />
         <Button onClick={decrementCounter} negative content='Decrement' />
+        <br />
+        <br />
+        <TestPlaceInput />
       </div>
     );
   }
