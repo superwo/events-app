80808438d2ca9f1b5aa16645d3df04b72ecf1f2b
Radu <avadanii@tiscali.it>
2019-10-03 09:52:30 +0300

    added google map integration

--- src/features/testarea/TestComponent.jsx
+++ src/features/testarea/TestComponent.jsx
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { incrementCounter, decrementCounter } from './testActions';
 import { Button } from 'semantic-ui-react';
+import { geocodeByAddress, getLatLng } from 'react-places-autocomplete';
 import TestPlaceInput from './TestPlaceInput';
+import SimpleMap from './SimpleMap';
 
 const mapState = state => ({
   data: state.test.data
@@ -14,6 +16,21 @@ const actions = {
 };
 
 export class TestComponent extends Component {
+  state = {
+    latlng: { lat: 59.95, lng: 30.33 }
+  };
+
+  handleSelect = address => {
+    geocodeByAddress(address)
+      .then(results => getLatLng(results[0]))
+      .then(latLng => {
+        this.setState({
+          latlng: latLng
+        });
+      })
+      .catch(error => console.error('Error', error));
+  };
+
   render() {
     const { data, incrementCounter, decrementCounter } = this.props;
     return (
@@ -24,7 +41,8 @@ export class TestComponent extends Component {
         <Button onClick={decrementCounter} negative content='Decrement' />
         <br />
         <br />
-        <TestPlaceInput />
+        <TestPlaceInput selectAddress={this.handleSelect} />
+        <SimpleMap key={this.state.latlng.lng} latlng={this.state.latlng} />
       </div>
     );
   }
