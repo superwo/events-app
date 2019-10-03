21f68c3a92560c91c585bf992de886d5b6ab3227
Radu <avadanii@tiscali.it>
2019-10-03 11:35:53 +0300

    added modal support

--- src/features/testarea/TestComponent.jsx
+++ src/features/testarea/TestComponent.jsx
@@ -5,6 +5,7 @@ import { Button } from 'semantic-ui-react';
 import { geocodeByAddress, getLatLng } from 'react-places-autocomplete';
 import TestPlaceInput from './TestPlaceInput';
 import SimpleMap from './SimpleMap';
+import { openModal } from '../modals/modalActions';
 
 const mapState = state => ({
   data: state.test.data
@@ -12,7 +13,8 @@ const mapState = state => ({
 
 const actions = {
   incrementCounter,
-  decrementCounter
+  decrementCounter,
+  openModal
 };
 
 export class TestComponent extends Component {
@@ -32,13 +34,18 @@ export class TestComponent extends Component {
   };
 
   render() {
-    const { data, incrementCounter, decrementCounter } = this.props;
+    const { data, incrementCounter, decrementCounter, openModal } = this.props;
     return (
       <div>
         <h1>Test Component</h1>
         <h3>The answer is: {data}</h3>
         <Button onClick={incrementCounter} positive content='Increment' />
         <Button onClick={decrementCounter} negative content='Decrement' />
+        <Button
+          onClick={() => openModal('TestModal', { data: 42 })}
+          color='teal'
+          content='Open Modal'
+        />
         <br />
         <br />
         <TestPlaceInput selectAddress={this.handleSelect} />
